d832ca269c8be3b337696a79560695ab39f1712e
myrabarbash <mira.barbash@gmail.com>
2020-10-04 16:50:40 +0300

    create app polls

--- project/blog/home/views.py
+++ project/blog/home/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'home/index.html',{})
 
+def contact(request):   
     return render(request, 'home/contact.html',{})
